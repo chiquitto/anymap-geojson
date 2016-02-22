6cb0b4b8dff8ee87e23f5b5a492b1b3b48916762
Alisson Chiquitto <chiquitto@gmail.com>
2016-02-22 11:54:13 -0300

    Ajustes em Fernando de Noronha

--- municipal-brazilian-geodata.php
+++ municipal-brazilian-geodata.php
@@ -14,24 +14,32 @@ $destino = realpath($destino);
 
 foreach ($di as $directory) {
     /* @var $directory DirectoryIterator */
-    
+
     if (!preg_match('/^([A-Z]{2})\.json$/', $directory->getFilename(), $matches)) {
         continue;
     }
     echo $directory->getPathname() . "\n";
-    
+
     $uf = $matches[1];
-    
+
     $o = json_decode(file_get_contents($directory->getPathname()), 1);
-    
+
     foreach ($o['features'] as & $features) {
         $features['properties'] = [
             'id' => (int) $features['properties']['GEOCODIGO'],
             'name' => $features['properties']['NOME']
         ];
+
+        // Ajuste para Fernando de Noronha
+        if ($features['properties']['id'] == 2605459) {
+            for ($i = 0; $i < count($features['geometry']['coordinates'][0]); $i++) {
+                $features['geometry']['coordinates'][0][$i][0] -= 3.5;
+                $features['geometry']['coordinates'][0][$i][1] -= 3.5;
+            }
+        }
     }
-    
+
     $json = json_encode($o);
-    
+
     file_put_contents("{$destino}/{$uf}.json", $json);
 }
